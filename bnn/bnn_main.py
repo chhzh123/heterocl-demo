6e919eefe1a310b462dfc21afffa460ab37d2b9d
chhzh123 <982270930@qq.com>
2020-05-02 18:18:46 +0800

    Update BNN (conv)

--- bnn/bnn_main.py
+++ bnn/bnn_main.py
@@ -7,7 +7,7 @@ hcl.init(hcl.Int())
 def build_bnn(input_image, weight_conv1):
     # first conv
     # 1*16*16
-    return hlib.op.nn.conv2d_nchw(input_image, weight_conv1)#, padding=[2,2]) # 64*16*16
+    return hlib.op.bnn.conv2d_nchw(input_image, weight_conv1)#, padding=[2,2]) # 64*16*16
     # return conv1
     # tanh1 = hlib.bnn.tanh(conv1, "tanh1")
     # pool1 = hlib.bnn.max_pool(tanh1, kernel=(2,2), stride=(2,2)) # 32*14*14
@@ -24,20 +24,30 @@ def build_bnn(input_image, weight_conv1):
     # # loss
     # return fc2
 
-A = hcl.placeholder((1,1,16,16), "A")
-weight_conv = hcl.placeholder((64,1,3,3), "conv")
-s = hcl.create_scheme([A, weight_conv], build_bnn)
-# s.downsize(, hcl.UInt(1))
-s = hcl.create_schedule_from_scheme(s)
-f = hcl.build(s, target=None)
-# print(f)
+batch_size = 1
 
+def build_bnn_inf(batch_size=batch_size, target=None):
+    # set up input/output placeholders
+    input_image = hcl.placeholder((batch_size,1,16,16), "A")
+    weight_conv = hcl.placeholder((64,1,3,3), "conv")
+    # create a quantization scheme
+    s = hcl.create_scheme([input_image, weight_conv], build_bnn)
+    # quantize the layers
+    s = hcl.create_schedule_from_scheme(s)
+    return hcl.build(s, target=target)
+
+f = build_bnn_inf()
+
+# prepare the numpy arrays for testing
 images = np.loadtxt("data/test_b.dat").astype(np.int).reshape((-1,1,16,16)) # 5000 images
 print("Loaded {} images".format(images.shape[0]))
 np_weight = np.loadtxt("data/weight_0b",delimiter=",").astype(np.int).reshape((64,1,3,3)) # 576
-np_A = images[:1,:,:,:]
-hcl_A = hcl.asarray(np_A)
+np_image = images[:1,:,:,:] # batch_size=1
+print(np_image.shape)
+print(np_image[0])
+print(np_weight[0])
+hcl_image = hcl.asarray(np_image)
 hcl_weight = hcl.asarray(np_weight)
-hcl_B = hcl.asarray(np.zeros((1,64,16,16)))
-f(hcl_A,hcl_weight,hcl_B)
-print(hcl_B)
+hcl_out = hcl.asarray(np.zeros((1,64,16,16)))
+f(hcl_image,hcl_weight,hcl_out)
+print(hcl_out.asnumpy()[0][0][:14,:14])
