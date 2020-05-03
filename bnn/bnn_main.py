8cc52fb0f7033ca3a656fc52799fcb021b6a115c
chhzh123 <982270930@qq.com>
2020-05-04 00:24:44 +0800

    Update BNN (use list to initialize)

--- bnn/bnn_main.py
+++ bnn/bnn_main.py
@@ -4,8 +4,10 @@ import numpy as np
 
 hcl.init(hcl.Float())
 
-def build_bnn(input_image, w_conv1, w_conv2,
+# compute declaration
+def build_bnn(input_image, w_conv1,
               gamma1, beta1, miu1, sigma1,
+              w_conv2,
               gamma2, beta2, miu2, sigma2,
               w_fc1, b_fc1,
               w_fc2, b_fc2): # 1*16*16
@@ -20,96 +22,52 @@ def build_bnn(input_image, w_conv1, w_conv2,
     fc2 = hlib.op.bnn.dense(fc1, w_fc2, b_fc2, False) # 512->10
     return fc2
 
+target = None
 batch_size = 100
 qtype_bit = hcl.Int()
 qtype_float = hcl.Float()
-
-def build_bnn_inf(batch_size=batch_size, target=None):
-    # set up input/output placeholders
-    input_image = hcl.placeholder((batch_size,1,16,16),"input_image",qtype_bit)
-
-    w_conv1 = hcl.placeholder((64,1,3,3),"conv1",qtype_bit)
-    gamma1 = hcl.placeholder((64,),"gamma1",qtype_float) # (channels,)
-    beta1 = hcl.placeholder((64,),"beta1",qtype_float)
-    miu1 = hcl.placeholder((64,),"miu1",qtype_float)
-    sigma1 = hcl.placeholder((64,),"sigma1",qtype_float)
-
-    w_conv2 = hcl.placeholder((128,64,3,3), "conv2",qtype_bit)
-    gamma2 = hcl.placeholder((128,),"gamma2",qtype_float)
-    beta2 = hcl.placeholder((128,),"beta2",qtype_float)
-    miu2 = hcl.placeholder((128,),"miu2",qtype_float)
-    sigma2 = hcl.placeholder((128,),"sigma2",qtype_float)
-
-    w_fc1 = hcl.placeholder((512,2048),"w_fc1",qtype_float)
-    b_fc1 = hcl.placeholder((512,),"b_fc1",qtype_float)
-    w_fc2 = hcl.placeholder((10,512),"w_fc2",qtype_float)
-    b_fc2 = hcl.placeholder((10,),"b_fc2",qtype_float)
-
-    # create a quantization scheme
-    scheme = hcl.create_scheme([input_image, w_conv1, w_conv2,
-                                gamma1, beta1, miu1, sigma1,
-                                gamma2, beta2, miu2, sigma2,
-                                w_fc1, b_fc1,
-                                w_fc2, b_fc2], build_bnn)
-    # quantize the layers
-    # scheme.quantize([build_bnn.conv1, build_bnn.conv2], qtype_bit)
-    s = hcl.create_schedule_from_scheme(scheme)
-    return hcl.build(s, target=target)
-
-f = build_bnn_inf()
 
 # prepare the numpy arrays for testing
 images = np.loadtxt("data/test_b.dat").astype(np.int).reshape((-1,1,16,16)) # 5000 images
 labels = np.loadtxt("data/label.dat").astype(np.int)
 num_images = images.shape[0]
 print("Loaded {} images".format(num_images))
-np_w_conv1 = np.loadtxt("data/weight_0b",delimiter=",").astype(np.int).reshape((1,64,3,3)) # 576
-np_w_conv1 = np_w_conv1.transpose(1,0,2,3)
-np_gamma1 = np.loadtxt("data/weight_1p",delimiter=",").astype(np.float)
-np_beta1 = np.loadtxt("data/weight_2p",delimiter=",").astype(np.float)
-np_miu1 = np.loadtxt("data/weight_3p",delimiter=",").astype(np.float)
-np_sigma1 = np.loadtxt("data/weight_4p",delimiter=",").astype(np.float)
-np_w_conv2 = np.loadtxt("data/weight_5b",delimiter=",").astype(np.int).reshape((64,128,3,3))
-np_w_conv2 = np_w_conv2.transpose(1,0,2,3)
-np_gamma2 = np.loadtxt("data/weight_6p",delimiter=",").astype(np.float)
-np_beta2 = np.loadtxt("data/weight_7p",delimiter=",").astype(np.float)
-np_miu2 = np.loadtxt("data/weight_8p",delimiter=",").astype(np.float)
-np_sigma2 = np.loadtxt("data/weight_9p",delimiter=",").astype(np.float)
-np_w_fc1 = np.loadtxt("data/weight_10b",delimiter=",").astype(np.float).reshape((2048,512))
-np_w_fc1 = np_w_fc1.T
-np_b_fc1 = np.loadtxt("data/weight_11p",delimiter=",").astype(np.float)
-np_w_fc2 = np.loadtxt("data/weight_12b",delimiter=",").astype(np.float).reshape((512,10))
-np_w_fc2 = np_w_fc2.T
-np_b_fc2 = np.loadtxt("data/weight_13p",delimiter=",").astype(np.float)
+params = {
+    "w_conv1": np.loadtxt("data/weight_0b",delimiter=",").astype(np.int).reshape((1,64,3,3)).transpose(1,0,2,3), # 576
+    "gamma1" : np.loadtxt("data/weight_1p",delimiter=",").astype(np.float),
+    "beta1"  : np.loadtxt("data/weight_2p",delimiter=",").astype(np.float),
+    "miu1"   : np.loadtxt("data/weight_3p",delimiter=",").astype(np.float),
+    "sigma1" : np.loadtxt("data/weight_4p",delimiter=",").astype(np.float),
+    "w_conv2": np.loadtxt("data/weight_5b",delimiter=",").astype(np.int).reshape((64,128,3,3)).transpose(1,0,2,3),
+    "gamma2" : np.loadtxt("data/weight_6p",delimiter=",").astype(np.float),
+    "beta2"  : np.loadtxt("data/weight_7p",delimiter=",").astype(np.float),
+    "miu2"   : np.loadtxt("data/weight_8p",delimiter=",").astype(np.float),
+    "sigma2" : np.loadtxt("data/weight_9p",delimiter=",").astype(np.float),
+    "w_fc1"  : np.loadtxt("data/weight_10b",delimiter=",").astype(np.float).reshape((2048,512)).T,
+    "b_fc1"  : np.loadtxt("data/weight_11p",delimiter=",").astype(np.float),
+    "w_fc2"  : np.loadtxt("data/weight_12b",delimiter=",").astype(np.float).reshape((512,10)).T,
+    "b_fc2"  : np.loadtxt("data/weight_13p",delimiter=",").astype(np.float)
+}
 
-hcl_w_conv1 = hcl.asarray(np_w_conv1, dtype=qtype_bit)
-hcl_gamma1 = hcl.asarray(np_gamma1, dtype=qtype_float)
-hcl_beta1 = hcl.asarray(np_beta1, dtype=qtype_float)
-hcl_miu1 = hcl.asarray(np_miu1, dtype=qtype_float)
-hcl_sigma1 = hcl.asarray(np_sigma1, dtype=qtype_float)
-
-hcl_w_conv2 = hcl.asarray(np_w_conv2, dtype=qtype_bit)
-hcl_gamma2 = hcl.asarray(np_gamma2, dtype=qtype_float)
-hcl_beta2 = hcl.asarray(np_beta2, dtype=qtype_float)
-hcl_miu2 = hcl.asarray(np_miu2, dtype=qtype_float)
-hcl_sigma2 = hcl.asarray(np_sigma2, dtype=qtype_float)
-
-hcl_w_fc1 = hcl.asarray(np_w_fc1, dtype=qtype_float)
-hcl_b_fc1 = hcl.asarray(np_b_fc1, dtype=qtype_float)
-hcl_w_fc2 = hcl.asarray(np_w_fc2, dtype=qtype_float)
-hcl_b_fc2 = hcl.asarray(np_b_fc2, dtype=qtype_float)
+# declare hcl array and placeholders
+hcl_array = []
+hcl_ph = []
+input_image = hcl.placeholder((batch_size,1,16,16),"input_image",qtype_bit)
+for name in params:
+    dtype = qtype_bit if "conv" in name else qtype_float
+    hcl_array.append(hcl.asarray(params[name],dtype=dtype))
+    hcl_ph.append(hcl.placeholder(params[name].shape,name,dtype=dtype))
 hcl_out = hcl.asarray(np.zeros((batch_size,10)))
+
+scheme = hcl.create_scheme([input_image] + hcl_ph, build_bnn)
+s = hcl.create_schedule_from_scheme(scheme)
+f = hcl.build(s, target=target)
 
 correct_sum = 0
 for i in range(num_images // batch_size):
     np_image = images[i*batch_size:(i+1)*batch_size]
     hcl_image = hcl.asarray(np_image, dtype=qtype_bit)
-    f(hcl_image, hcl_w_conv1, hcl_w_conv2,
-        hcl_gamma1, hcl_beta1, hcl_miu1, hcl_sigma1,
-        hcl_gamma2, hcl_beta2, hcl_miu2, hcl_sigma2,
-        hcl_w_fc1, hcl_b_fc1,
-        hcl_w_fc2, hcl_b_fc2,
-        hcl_out)
+    f(hcl_image, *hcl_array, hcl_out)
     prediction = np.argmax(hcl_out.asnumpy(), axis=1)
     correct_sum += np.sum(np.equal(prediction, labels[i*batch_size:(i+1)*batch_size]))
     if (i+1) % 10 == 0:
