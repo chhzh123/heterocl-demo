539766fa74252e3ebd8a7eccadcd1181dd0cf413
chhzh123 <982270930@qq.com>
2020-05-14 01:36:27 +0800

    [test] Add test case for hcl.select

--- simple_add/select_test/select_test.cpp
+++ simple_add/select_test/select_test.cpp
@@ -4,11 +4,12 @@
 #include <math.h>
 #include <stdint.h>
 
-void default_function(ap_int<8> A[8*8], ap_fixed<16, 4> B[8*8], ap_int<32> C[8*8]) {
+void default_function(ap_uint<1> A[8*8], ap_int<32> B[8*8]) {
   ap_int<32> _top;
   for (ap_int<32> y = 0; y < 8; ++y) {
     for (ap_int<32> x = 0; x < 8; ++x) {
-      C[(x + (y * 8))] = ((ap_int<32>)((x < 4) ? ((ap_fixed<32, 0>)((ap_int<16>)A[(x + (y * 8))])) : ((ap_fixed<32, 0>)((ap_int<16>)B[(x + (y * 8))]))));
+      B[(x + (y * 8))] = ((ap_int<32>)((x < 4) ? (((ap_uint<32>)A[(x + (y * 8))])) : (0U)));
     }
   }
 }
+
--- simple_add/select_test/select_test.py
+++ simple_add/select_test/select_test.py
@@ -1,12 +1,27 @@
 import heterocl as hcl
 import numpy as np
 
-A = hcl.placeholder((8, 8), "A")
-B = hcl.placeholder((8, 8), "B", dtype=hcl.Fixed(16,12))
-def kernel(A, B):
-    return hcl.compute((8, 8), lambda y, x: 
-        hcl.select(x < 4, A[y][x], B[y][x]), "C")
-s = hcl.create_scheme([A, B], kernel)
-s = hcl.create_schedule_from_scheme(s)
-f = hcl.build(s, target="vhls")
-print(f)
+def test1():
+	A = hcl.placeholder((8, 8), "A")
+	B = hcl.placeholder((8, 8), "B", dtype=hcl.Fixed(16,12))
+	def kernel(A, B):
+	    return hcl.compute((8, 8), lambda y, x: 
+	        hcl.select(x < 4, A[y][x], B[y][x]), "C")
+	s = hcl.create_scheme([A, B], kernel)
+	s = hcl.create_schedule_from_scheme(s)
+	f = hcl.build(s, target="vhls")
+	print(f)
+
+def test2():
+	A = hcl.placeholder((8, 8), "A", dtype=hcl.UInt(1))
+	def kernel(A):
+	    return hcl.compute((8, 8), lambda y, x: 
+	        hcl.select(x < 4, A[y][x], 0), "B")
+	s = hcl.create_scheme([A], kernel)
+	s = hcl.create_schedule_from_scheme(s)
+	f = hcl.build(s, target="vhls")
+	with open("select_test.cpp","w") as outfile:
+		outfile.write(f)
+
+if __name__ == '__main__':
+	test2()
