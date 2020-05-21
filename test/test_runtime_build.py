8ba758d627e9da05d59d14d33075fd370b6c3ca5
ChenHZ <chenhzh37@mail2.sysu.edu.cn>
2020-05-21 22:59:42 +0800

    [test] Vivado runtime build changes API

--- test/test_runtime_build.py
+++ test/test_runtime_build.py
@@ -2,70 +2,47 @@ import heterocl as hcl
 from itertools import permutations
 import os
 import numpy as np
+import heterocl.report as report
 
 def test_vivado_hls():
     if os.system("which vivado_hls >> /dev/null") != 0:
         return 
 
-    hcl.init()
-    A = hcl.placeholder((10, 32), "A")
-    def kernel(A):
-        B = hcl.compute(A.shape, lambda *args : A[args] + 1, "B")
-        C = hcl.compute(A.shape, lambda *args : B[args] + 1, "C")
-        D = hcl.compute(A.shape, lambda *args : C[args] * 2, "D")
-        return D
-    
-    target = hcl.platform.zc706
-    s = hcl.create_schedule([A], kernel)
-    s.to(kernel.B, target.xcel)
-    s.to(kernel.C, target.host)
-    target.config(compile="vivado_hls", mode="sw_sim")
-    f = hcl.build(s, target)
+    def test_hls(target_mode):
+        hcl.init()
+        A = hcl.placeholder((10, 32), "A")
+        def kernel(A):
+            B = hcl.compute(A.shape, lambda *args : A[args] + 1, "B")
+            C = hcl.compute(A.shape, lambda *args : B[args] + 1, "C")
+            D = hcl.compute(A.shape, lambda *args : C[args] * 2, "D")
+            return D
+        
+        target = hcl.platform.aws_f1
+        s = hcl.create_schedule([A], kernel)
+        s.to(kernel.B, target.xcel)
+        s.to(kernel.C, target.host)
+        target.config(compile="vivado_hls", mode=target_mode)
+        f = hcl.build(s, target)
 
-    np_A = np.random.randint(10, size=(10,32))
-    np_B = np.zeros((10,32))
+        np_A = np.random.randint(10, size=(10,32))
+        np_B = np.zeros((10,32))
 
-    hcl_A = hcl.asarray(np_A)
-    hcl_B = hcl.asarray(np_B, dtype=hcl.Int(32))
-    f(hcl_A, hcl_B)
-    print(f)
-    ret_B = hcl_B.asnumpy()
-    print(ret_B)
-    print((np_A+2)*2)
+        hcl_A = hcl.asarray(np_A)
+        hcl_B = hcl.asarray(np_B, dtype=hcl.Int(32))
+        f(hcl_A, hcl_B)
+        ret_B = hcl_B.asnumpy()
 
-    for i in range(0, 10):
-      for j in range(0, 32):
-        assert ret_B[i, j] == (np_A[i, j] + 2) *2
+        if target_mode == "csyn":
+            report = f.report("csyn")
+            assert "ReportVersion" in report
+        else:
+            for i in range(0, 10):
+                for j in range(0, 32):
+                    assert ret_B[i, j] == (np_A[i, j] + 2) *2
 
-def test_vivado_hls_exe():
-    if os.system("which vivado_hls >> /dev/null") != 0:
-        return 
-
-    hcl.init()
-    A = hcl.placeholder((10, 32), "A")
-    def kernel(A):
-        B = hcl.compute(A.shape, lambda *args : A[args] + 1, "B")
-        C = hcl.compute(A.shape, lambda *args : B[args] + 1, "C")
-        D = hcl.compute(A.shape, lambda *args : C[args] * 2, "D")
-        return D
-    
-    target = hcl.platform.zc706
-    s = hcl.create_schedule([A], kernel)
-    s.to(kernel.B, target.xcel)
-    s.to(kernel.C, target.host)
-    target.config(compile="vivado_hls", mode="sw_exe")
-    f = hcl.build(s, target)
-
-    np_A = np.random.randint(10, size=(10,32))
-    np_B = np.zeros((10,32))
-    hcl_A = hcl.asarray(np_A)
-    hcl_B = hcl.asarray(np_B, dtype=hcl.Int(32))
-    f(hcl_A, hcl_B)
-    print(f)
-
-    report = f.hls_report()
-    assert "ReportVersion" in report
+    test_hls("csim")
+    test_hls("csyn")
 
 if __name__ == '__main__':
-    # test_vivado_hls()
-    test_vivado_hls_exe()
+    test_vivado_hls()
+    # report.parse_xml("project",True)
