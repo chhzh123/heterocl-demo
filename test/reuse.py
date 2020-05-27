0a9084dbbdc21b41f7d332f5649e67a736730dce
ChenHZ <chenhzh37@mail2.sysu.edu.cn>
2020-05-27 23:51:46 +0800

    [test] Update reuse

--- test/reuse.py
+++ test/reuse.py
@@ -26,24 +26,24 @@ def test_reuse_blur_x():
     B = hcl.compute((10, 8), lambda y, x: A[y, x] + A[y, x+1] + A[y, x+2])
     s = hcl.create_schedule([A, B])
     RB = s.reuse_at(A, s[B], B.axis[1])
-    # f = hcl.build(s)
+    f = hcl.build(s)
 
-    # np_A = np.random.randint(0, 10, size=(10, 10))
-    # np_B = np.zeros((10, 8), dtype="int")
-    # np_C = np.zeros((10, 8), dtype="int")
+    np_A = np.random.randint(0, 10, size=(10, 10))
+    np_B = np.zeros((10, 8), dtype="int")
+    np_C = np.zeros((10, 8), dtype="int")
 
-    # for y in range(0, 10):
-    #     for x in range(0, 8):
-    #         np_C[y][x] = np_A[y][x] + np_A[y][x+1] + np_A[y][x+2]
+    for y in range(0, 10):
+        for x in range(0, 8):
+            np_C[y][x] = np_A[y][x] + np_A[y][x+1] + np_A[y][x+2]
 
-    # hcl_A = hcl.asarray(np_A)
-    # hcl_B = hcl.asarray(np_B)
+    hcl_A = hcl.asarray(np_A)
+    hcl_B = hcl.asarray(np_B)
 
-    # f(hcl_A, hcl_B)
+    f(hcl_A, hcl_B)
 
-    # np_B = hcl_B.asnumpy()
+    np_B = hcl_B.asnumpy()
 
-    # assert np.array_equal(np_B, np_C)
+    assert np.array_equal(np_B, np_C)
 
 test_reuse_blur_x()
 
