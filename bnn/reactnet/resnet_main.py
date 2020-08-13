d63c2d3a4da97216ac9e4e945ac01f9a1036dab2
HZ Chen <chenhzh37@mail2.sysu.edu.cn>
2020-08-13 22:33:33 +0800

    [bnn] Update ResNet (correct conv)

--- bnn/reactnet/resnet_main.py
+++ bnn/reactnet/resnet_main.py
@@ -8,16 +8,20 @@ import torchvision
 import torchvision.transforms as transforms
 
 target = None
-batch_size = 100
+batch_size = 5
 qtype_bit = hcl.UInt(1) # weights
-qtype_int = hcl.Int(6) # not unsigned!
-qtype_float = hcl.Fixed(20,10)
-qtype_packed = hcl.UInt(32)
+# qtype_bit = hcl.Float()
+# qtype_int = hcl.Int(6) # not unsigned!
+qtype_int = hcl.Int(32)
+# qtype_int = hcl.Float()
+# qtype_float = hcl.Fixed(20,10)
+qtype_float = hcl.Float()
+# qtype_packed = hcl.UInt(32)
 
-def RSign(data, alpha, name="rsign", dtype=hcl.UInt(2)):
+def RSign(data, alpha, name="rsign", dtype=hcl.UInt(1)):
     assert data.shape[1] == alpha.shape[0]
     return hcl.compute(data.shape, lambda nn, cc, ww, hh: 
-                        hcl.select(data[nn,cc,ww,hh] > alpha[cc], 1, 0),
+                        hcl.select(data[nn,cc,ww,hh] + alpha[cc] > 0, 1, 0),
                         name=name, dtype=dtype)
 
 def RPReLU(data, x0, y0, beta, name="rprelu", dtype=None):
@@ -40,9 +44,9 @@ class BasicBlock():
         self.params["rsign1"] = params[6]
         self.params["rsign2"] = params[7]
         self.params["conv1"] = params[8]
-        self.params["bn1"] = params[9:13] # ignore "num_batches_tracked"
-        self.params["conv2"] = params[14]
-        self.params["bn2"] = params[15:19]
+        self.params["bn1"] = params[9:13]
+        self.params["conv2"] = params[13]
+        self.params["bn2"] = params[14:18]
         self.stride = stride
         self.flag = in_planes != planes
         self.name = name
@@ -51,10 +55,13 @@ class BasicBlock():
         return self.forward(x)
 
     def forward(self, x):
+        # return self.params["rsign1"]
         # 1st residual block
-        rsign1 = RSign(x, self.params["rsign1"], name=self.name+"_rsign1")
-        conv1 = bnn.conv2d_nchw(rsign1, self.params["conv1"], padding=[1,1], name=self.name+"_conv1") # no bias!
-        bn1, _, _ = nn.batch_norm(conv1, *self.params["bn1"], name=self.name+"_bn1")
+        rsign1 = RSign(x, self.params["rsign1"], name=self.name+"_rsign1", dtype=qtype_int)
+        conv1 = bnn.conv2d_nchw(rsign1, self.params["conv1"], padding=[1,1], strides=[self.stride,self.stride], name=self.name+"_conv1", out_dtype=qtype_int) # no bias!
+        # conv1 = nn.conv2d_nchw(rsign1, self.params["conv1"], padding=[1,1], strides=[self.stride,self.stride], name=self.name+"_conv1", out_dtype=qtype_int)
+        return conv1
+        bn1, _, _ = nn.batch_norm(conv1, *self.params["bn1"], name=self.name+"_bn1",dtype=qtype_float)
         if self.stride != 1 or self.flag:
             avgpool = nn.avg_pool2d_nchw(bn1, pooling=[2,2],
                                          stride=[2,2], padding=[0,0],
@@ -63,21 +70,21 @@ class BasicBlock():
             shape = avgpool.shape
             shortcut = hcl.compute((shape[0], shape[1]*2, shape[2], shape[3]),
                                     lambda nn, cc, ww, hh: avgpool[nn, cc % shape[1], ww, hh],
-                                    name=self.name+"_concat")
+                                    name=self.name+"_concat",dtype=qtype_float)
         else:
             shortcut = x
         residual1 = hcl.compute(bn1.shape, lambda nn, cc, ww, hh:
                                 bn1[nn, cc, ww, hh] + shortcut[nn, cc, ww, hh],
-                                name=self.name+"_residual1")
+                                name=self.name+"_residual1",dtype=qtype_float)
         # 2nd residual block
-        rprelu1 = RPReLU(residual1, *self.params["rprelu1"], name=self.name+"_rprelu1")
-        rsign2 = RSign(rprelu1, self.params["rsign2"], name=self.name+"_rsign2")
-        conv2 = bnn.conv2d_nchw(rsign2, self.params["conv2"], padding=[1,1], name=self.name+"_conv2")
-        bn2, _, _ = nn.batch_norm(conv2, *self.params["bn2"], name=self.name+"_bn2")
+        rprelu1 = RPReLU(residual1, *self.params["rprelu1"], name=self.name+"_rprelu1",dtype=qtype_float)
+        rsign2 = RSign(rprelu1, self.params["rsign2"], name=self.name+"_rsign2",dtype=qtype_bit)
+        conv2 = bnn.conv2d_nchw(rsign2, self.params["conv2"], strides=[1,1], padding=[1,1], name=self.name+"_conv2",out_dtype=qtype_int)
+        bn2, _, _ = nn.batch_norm(conv2, *self.params["bn2"], name=self.name+"_bn2",dtype=qtype_float)
         residual2 = hcl.compute(rprelu1.shape, lambda nn, cc, ww, hh:
                                 bn2[nn, cc, ww, hh] + rprelu1[nn, cc, ww, hh],
-                                name=self.name+"_residual2")
-        rprelu2 = RPReLU(residual2, *self.params["rprelu2"], name=self.name+"_rprelu2")
+                                name=self.name+"_residual2",dtype=qtype_float)
+        rprelu2 = RPReLU(residual2, *self.params["rprelu2"], name=self.name+"_rprelu2",dtype=qtype_float)
         return rprelu2
 
 class Sequential():
@@ -92,6 +99,7 @@ class Sequential():
         out = x
         for layer in self.layers:
             out = layer(out)
+            return out
         return out
 
 class ResNet():
@@ -100,11 +108,11 @@ class ResNet():
         self.in_planes = 16
         self.params = dict()
         self.params["conv1"] = params[0]
-        self.params["bn1"] = params[1:5] # ignore "num_batches_tracked"
-        self.params["layer1"] = params[6:66]
-        self.params["layer2"] = params[66:126]
-        self.params["layer3"] = params[126:186]
-        self.params["linear"] = params[186:]
+        self.params["bn1"] = params[1:5]
+        self.params["layer1"] = params[5:59]
+        self.params["layer2"] = params[59:113]
+        self.params["layer3"] = params[113:167]
+        self.params["linear"] = params[167:]
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1, params=self.params["layer1"], id=0)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2, params=self.params["layer2"], id=1)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2, params=self.params["layer3"], id=2)
@@ -113,7 +121,7 @@ class ResNet():
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for i, stride in enumerate(strides):
-            layers.append(block(self.in_planes, planes, stride, params[i*20:(i+1)*20], name="bb{}".format(id*3+i)))
+            layers.append(block(self.in_planes, planes, stride, params[i*18:(i+1)*18], name="bb{}".format(id*3+i)))
             self.in_planes = planes
 
         return Sequential(*layers)
@@ -122,13 +130,15 @@ class ResNet():
         return self.forward(x)
 
     def forward(self, x):
-        conv1 = nn.conv2d_nchw(x, self.params["conv1"], strides=[1, 1], padding=[1, 1], name="out_conv")
-        layer1 = self.layer1(conv1)
+        conv1 = nn.conv2d_nchw(x, self.params["conv1"], strides=[1, 1], padding=[1, 1], name="out_conv", out_dtype=qtype_float)
+        bn, _, _ = nn.batch_norm(conv1, *self.params["bn1"], name="out_bn",dtype=qtype_float)
+        layer1 = self.layer1(bn)
+        return layer1
         layer2 = self.layer2(layer1)
         layer3 = self.layer3(layer2)
         avgpool = nn.avg_pool2d_nchw(layer3, pooling=[2, 2], stride=[2, 2], padding=[0, 0], name="avgpool")
         flat = bnn.flatten(avgpool, name="flatten")
-        out = nn.dense(flat, self.params["linear"][0], bias=self.params["linear"][1], name="fc")
+        out = nn.dense(flat, self.params["linear"][0], bias=self.params["linear"][1], name="fc", out_dtype=qtype_float)
         return out
 
 def build_resnet20(*params): # params are placeholders here
@@ -136,11 +146,12 @@ def build_resnet20(*params): # params are placeholders here
     return resnet(params[0])
 
 # declare hcl placeholders
-def build_resnet20_inf(images, params, batch_size=batch_size, target=target):
+def build_resnet20_inf(params, batch_size=batch_size, target=target):
     hcl_ph = []
-    input_image = hcl.placeholder((batch_size,*images.shape[1:]),"input_image")
+    input_image = hcl.placeholder((batch_size,3,32,32),"input_image",dtype=qtype_float)
     for name in params:
-        hcl_ph.append(hcl.placeholder(params[name].shape,name))
+        dtype = qtype_bit if "conv" in name and "layer" in name else qtype_float
+        hcl_ph.append(hcl.placeholder(params[name].shape,name,dtype=dtype))
 
     # build the network
     scheme = hcl.create_scheme([input_image] + hcl_ph, build_resnet20)
@@ -162,38 +173,59 @@ def load_cifar10():
         ])
     test_set = torchvision.datasets.CIFAR10(root='.', train=False,
                                            download=True, transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=10,
+                                             shuffle=False,
+                                             num_workers=2)
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    return test_set.data, np.array(test_set.targets)
+    return test_loader
 
 params = torch.load("pretrained-models/model_react-resnet20.pt", map_location=torch.device("cpu"))
 print("Loading the data.")
-images, labels = load_cifar10()
-num_images = len(images)
+test_loader = load_cifar10()
+new_params = dict()
 for key in params:
-    if "rprelu" in key or "binarize" in key:
-        params[key] = params[key].reshape(-1)
+    if "num_batches_tracked" in key:
+        continue
+    elif "rprelu" in key or "binarize" in key:
+        new_params[key] = np.array(params[key]).reshape(-1)
+    elif "conv" in key and "layer" in key:
+        temp = np.sign(params[key])
+        temp[temp < 0] = 0 # change from {-1,1} to {0,1}
+        new_params[key] = temp
+        print(new_params[key][0,0,:,:])
+    else:
+        new_params[key] = np.array(params[key])
+params = new_params
 
-hls_code = build_resnet20_inf(images, params, target="vhls")
+hls_code = build_resnet20_inf(params, target="vhls")
 with open("vhls_code.cpp","w") as outfile:
     outfile.write(hls_code)
 print("Finish generating Vivado HLS code.")
 
-resnet20 = build_resnet20_inf(images, params)
+resnet20 = build_resnet20_inf(params)
 print("Finish building function.")
 
 hcl_array = []
 for name in params:
-    hcl_array.append(hcl.asarray(params[name],dtype=qtype_float))
+    dtype = qtype_bit if "conv" in name and "layer" in name else qtype_float
+    # print(name,params[name].dtype,dtype,params[name].shape)
+    hcl_array.append(hcl.asarray(params[name],dtype=dtype))
+    if name == "layer1.0.conv1.weight":
+        print(hcl_array[-1].asnumpy()[0,0,:,:])
 hcl_out = hcl.asarray(np.zeros((batch_size,10)).astype(np.float),dtype=qtype_float)
+hcl_out = hcl.asarray(np.zeros((16,16,3,3)),dtype=qtype_int)
+hcl_out = hcl.asarray(np.zeros((batch_size,16,32,32)),dtype=qtype_int)
 
 correct_sum = 0
-for i in range(num_images // batch_size):
-    np_image = images[i*batch_size:(i+1)*batch_size]
+for i, (images, labels) in enumerate(test_loader):
+    np_image = np.array(images)
     hcl_image = hcl.asarray(np_image, dtype=qtype_float)
     resnet20(hcl_image, *hcl_array, hcl_out)
+    print(hcl_out.asnumpy()[0,0,:,:])
     prediction = np.argmax(hcl_out.asnumpy(), axis=1)
-    correct_sum += np.sum(np.equal(prediction, labels[i*batch_size:(i+1)*batch_size]))
-    if (i+1) % 10 == 0:
-        print("Done {} batches.".format(i+1))
+    correct_sum += np.sum(np.equal(prediction, labels))
+    print("Done {} batches.".format(i+1))
+    # if (i+1) % 10 == 0:
+    #     print("Done {} batches.".format(i+1))
 print("Testing accuracy: {}".format(correct_sum / float(num_images)))
