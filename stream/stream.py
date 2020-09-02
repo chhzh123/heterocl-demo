f02d6eab81790d955756561127aa807b4adad7d3
HZ Chen <chenhzh37@mail2.sysu.edu.cn>
2020-09-03 00:48:23 +0800

    [stream] Add test for imperative code

--- stream/stream.py
+++ stream/stream.py
@@ -154,6 +154,30 @@ def test_consecutive():
     hcl_C = hcl.asarray(np_C)
     f(hcl_A, hcl_C)
 
+def test_duplicated():
+    A = hcl.placeholder((10,), "A")
+
+    def kernel(A):
+        B = hcl.compute(A.shape, 
+                lambda i: A[i] + 1, "B")
+        C = hcl.compute(B.shape,
+                lambda i: B[i] + 1, "C")
+        return C
+
+    target = hcl.platform.zc706
+    target.config(compile="vivado_hls", mode="csyn")
+    s = hcl.create_schedule([A], kernel)
+    s.to([A], target.xcel)
+    s.to(kernel.C, target.host)
+    s.to(kernel.B, s[kernel.C])
+    s.to(kernel.B, s[kernel.C]) # duplicated streaming
+    f = hcl.build(s, target)
+    np_A = np.zeros((10,))
+    np_C = np.zeros((10,))
+    hcl_A = hcl.asarray(np_A)
+    hcl_C = hcl.asarray(np_C)
+    f(hcl_A, hcl_C)
+
 def test_residual2():
     A = hcl.placeholder((10,), "A")
 
@@ -204,10 +228,84 @@ def test_complex():
     hcl_C = hcl.asarray(np_C)
     f(hcl_A, hcl_C)
 
+def test_hierarchy():
+    dtype = hcl.Float()
+    A = hcl.placeholder((4, 4), "A", dtype)
+
+    def kernel(A):
+
+        def func(data):
+            out = hcl.compute((4, 4),lambda x, y: 0, "out", dtype)
+            with hcl.Stage("S"):
+                with hcl.for_(0, 4, name="i") as i:
+                    with hcl.for_(0, 4, name="j") as j:
+                        out[i, j] = data[i, j] + 1
+            return out
+
+        B = func(A)
+        C = hcl.compute((4,4), lambda i, j: B[i, j] + 1, "C")
+        return C
+
+    s = hcl.create_schedule([A], kernel)
+
+    target = hcl.platform.zc706
+    target.config(compile="vivado_hls",mode="csyn")
+    s.to(A, target.xcel)
+    s.to(kernel.C, target.host)
+    f = hcl.build(s, target=target)
+    np_A = np.random.randint(0, 10, A.shape)
+    hcl_A = hcl.asarray(np_A,dtype)
+    hcl_B = hcl.asarray(np.zeros((4, 4),np.float),dtype)
+    f(hcl_A, hcl_B)
+
+def test_imperative():
+    dtype = hcl.Float()
+    A = hcl.placeholder((4, 4), "A", dtype)
+
+    def kernel(A):
+
+        def pool(data):
+            out = hcl.compute((2, 2),lambda x, y: 0, "out", dtype)
+            with hcl.Stage("S"):
+                LB = hcl.compute((2, 4),lambda x, y: 0, "LB", dtype)
+                with hcl.for_(0, 2, name="x") as x:
+                    with hcl.for_(0, 2, name="y") as y:
+                        with hcl.for_(0, 2, name="LB_i") as LB_i:
+                            with hcl.for_(0, 4, name="LB_j") as LB_j:
+                                LB[LB_i, LB_j] = data[x * 2 + LB_i, LB_j]
+                        val = hcl.scalar(0,"val")
+                        with hcl.for_(0, 2, name="r") as r:
+                            with hcl.for_(0, 2, name="c") as c:
+                                val.v += LB[r, y * 2 + c]
+                        out[x, y] = val / 4
+            return out
+
+        B = hcl.compute((4,4), lambda i, j: A[i, j] + 1, "B")
+        C = pool(B)
+        D = hcl.compute((2,2), lambda i, j: C[i, j] + 1, "D")
+        return C
+
+    s = hcl.create_schedule([A], kernel)
+    s[kernel.S].pipeline(kernel.S.axis[1])
+    s.partition(A, dim=2)
+
+    target = hcl.platform.zc706
+    target.config(compile="vivado_hls",mode="csyn",project="pool.prj")
+    s.to(A, target.xcel)
+    s.to(kernel.S.B, target.host)
+    f = hcl.build(s, target=target)
+    np_A = np.random.randint(0, 10, A.shape)
+    hcl_A = hcl.asarray(np_A,dtype)
+    hcl_B = hcl.asarray(np.zeros((2, 2),np.float),dtype)
+    f(hcl_A, hcl_B)
+
 if __name__ == "__main__":
     # test_inter_stage()
     # test_simple_reuse()
     # test_residual()
     # test_consecutive()
+    # test_duplicated()
     # test_residual2()
-    test_complex()
+    # test_complex()
+    # test_imperative()
+    test_hierarchy()
