af1d024f462daa5cf2b4b2818ef671b57b00ed28
HZ Chen <chenhzh37@mail2.sysu.edu.cn>
2020-09-05 12:02:32 +0800

    [stream] Add interface test

--- stream/stream.py
+++ stream/stream.py
@@ -1,61 +1,5 @@
 import heterocl as hcl
 import numpy as np
-
-# hcl.init()
-# target = hcl.platform.zc706
-# initiation_interval = 4
-
-# a = hcl.placeholder((10, 20), name="a")
-# b = hcl.placeholder((10, 20), name="b")
-# c = hcl.placeholder((10, 20), name="c") 
-# d = hcl.placeholder((10, 20), name="d")
-# e = hcl.placeholder((10, 20), name="e")
-
-# def add_mul(a, b, c, d, e):
-#     @hcl.def_([a.shape, b.shape, c.shape])
-#     def ret_add(a, b, c):
-#         with hcl.for_(0, a.shape[0]) as i:
-#             with hcl.for_(0, a.shape[1]) as j:
-#                 c[i, j] = a[i, j] + b[i, j]
-
-#     @hcl.def_([c.shape, d.shape, e.shape])
-#     def ret_mul(c, d, e):
-#         # hcl.update(c, lambda x, y: a[x, y] * b[x, y], 'c_mul')
-#         with hcl.for_(0, c.shape[0]) as i:
-#             with hcl.for_(0, c.shape[1]) as j:
-#                 e[i, j] = c[i, j] * d[i, j]
-
-#     ret_add(a, b, c)
-#     ret_mul(c, d, e)
-
-# # compute customization
-# s = hcl.create_schedule([a, b, c, d, e], add_mul)
-# # op1 = add_mul.ret_add.c
-# # op2 = add_mul.ret_mul.c
-# # s[op1].pipeline(op1.axis[0], initiation_interval)
-
-# # stream into modules / device
-# a0, b0 = s.to([a, b], target.xcel)
-# d0 = s.to(d, target.xcel)
-# #s.partition(b0, dim=2, factor=2)
-# s.to([a0, b0], s[add_mul.ret_add])
-# s.to(d0, s[add_mul.ret_mul])
-
-# # within device move producer to consumer
-# s.to(c, s[add_mul.ret_mul],
-#         s[add_mul.ret_add], depth=10)
-
-# # return tensor for inter-device move
-# # e0 = s.stream_to(e, hcl.CPU('riscv'))
-
-# # print(add_mul.ret_mul._buf, c._buf)
-# print(hcl.lower(s))
-# code = hcl.build(s, target)
-# print(code)
-# # 
-# # with open("example.cl", "w") as f:
-# #   f.write(code)
-# #   f.close()
  
 def test_inter_stage():
     A = hcl.placeholder((10, 32), "A")
@@ -329,6 +273,32 @@ def test_dataflow():
     hcl_D = hcl.asarray(np_D)
     f(hcl_A, hcl_D)
 
+def test_interface():
+    A = hcl.placeholder((10,), "A")
+    W = hcl.placeholder((10,), "W")
+
+    def kernel(A, W):
+        B = hcl.compute(A.shape, 
+                lambda i: A[i] + 1, "B")
+        C = hcl.compute(B.shape,
+                lambda i: B[i] + W[i], "C")
+        return C
+
+    target = hcl.platform.aws_f1
+    target.config(compile="vitis", mode="hw_exe", project="project-vitis.prj")
+    s = hcl.create_schedule([A, W], kernel)
+    s.to([A, W], target.xcel)
+    s.to(kernel.C, target.host)
+    s.to(kernel.B, s[kernel.C])
+    f = hcl.build(s, target)
+    np_A = np.zeros((10,))
+    np_W = np.zeros((10,))
+    np_C = np.zeros((10,))
+    hcl_A = hcl.asarray(np_A)
+    hcl_W = hcl.asarray(np_W)
+    hcl_C = hcl.asarray(np_C)
+    f(hcl_A, hcl_W, hcl_C)
+
 if __name__ == "__main__":
     # test_inter_stage()
     # test_simple_reuse()
@@ -339,4 +309,5 @@ if __name__ == "__main__":
     # test_complex()
     # test_imperative()
     # test_hierarchy()
-    test_dataflow()
+    # test_dataflow()
+    test_interface()
