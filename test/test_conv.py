022d36c72b1626f630f3ca542c2afb625dd853da
HZ Chen <chenhzh37@mail2.sysu.edu.cn>
2020-09-13 17:25:29 +0800

    [test] Update test_conv

--- test/test_conv.py
+++ test/test_conv.py
@@ -1,5 +1,6 @@
 import heterocl as hcl
 import numpy as np
+import hlib.op.bnn as bnn
 
 SIZE = 6
 KERNEL_SIZE = 3
@@ -20,7 +21,7 @@ def conv1():
     WB = s.reuse_at(LB, s[kernel.B], kernel.B.axis[1], "WB")
     # s.partition(LB, dim=1)
     # s.partition(WB)
-    s.partition(kernel.F)
+    s.partition(kernel.F, hcl.Partition.Cyclic, factor=KERNEL_SIZE)
     s[kernel.B].pipeline(kernel.B.axis[1])
 
     target = hcl.platform.zc706
@@ -45,7 +46,10 @@ def conv2():
     WB = s.reuse_at(LB, s[kernel.B], kernel.B.axis[1], "WB")
     # s.partition(LB, dim=1)
     # s.partition(WB)
-    s.partition(F)
+    F_ = s.partition(F, hcl.Partition.Cyclic, factor=KERNEL_SIZE, dim=2)
+    s.partition(F_, hcl.Partition.Cyclic, factor=KERNEL_SIZE, dim=1)
+    # s.partition(F, hcl.Partition.Cyclic, factor=3, dim=2)
+    # s.partition(F, hcl.Partition.Cyclic, factor=3, dim=1)
     s[kernel.B].pipeline(kernel.B.axis[1])
     
     target = hcl.platform.zc706
@@ -81,7 +85,31 @@ def conv3():
     hcl_B = hcl.asarray(np.zeros((SIZE-KERNEL_SIZE+1, SIZE-KERNEL_SIZE+1)))
     f(hcl_A, hcl_B)
 
+def conv4():
+    dtype = hcl.UInt(4)
+    A = hcl.placeholder((1, 1, 8, 8), name="A", dtype=dtype)
+    F = hcl.placeholder((16, 1, 3, 3), name="F", dtype=dtype)
+
+    def kernel(A, F):
+        return bnn.conv2d_nchw(A, F, padding=[1,1], name="conv1", out_dtype=hcl.UInt(16))
+
+    s = hcl.create_schedule([A, F], kernel)
+    s_conv = kernel.conv1
+    LB = s.reuse_at(kernel.conv1_pad._op, s[s_conv], s_conv.axis[2], "LB")
+    WB = s.reuse_at(LB, s[s_conv], s_conv.axis[3], "WB")
+
+    target = hcl.platform.zc706
+    target.config(compile="vivado_hls",mode="csim",project="conv")
+    s.to([A, F], target.xcel)
+    s.to(kernel.conv1, target.host)
+    f = hcl.build(s, target=target)
+    hcl_A = hcl.asarray(np.random.randint(0, 10, A.shape))
+    hcl_F = hcl.asarray(np.random.randint(0, 10, F.shape))
+    hcl_B = hcl.asarray(np.zeros((16,8,8,8)))
+    f(hcl_A, hcl_F, hcl_B)
+
 if __name__ == "__main__":
-    conv1()
-    conv2()
-    conv3()
+    # conv1()
+    # conv2()
+    # conv3()
+    conv4()
